216c2d90f249c623dbe20b7486e09c05b876e07b
alpipego <alpipego@gmail.com>
2015-11-17 13:43:55 +0100

    removed path variable and styles

--- header.php
+++ header.php
@@ -1,7 +1,3 @@
-<?php
-    $path = new \Theme\Functions\Path();
-
-?>
 <!DOCTYPE html>
 <!--[if IE 8]> <html class="no-js lt-ie9" lang="en"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
@@ -9,22 +5,13 @@
 <head>
 	<meta charset="utf-8" />
     <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1, maximum-scale=1">
-	<title><?php bloginfo( 'name' ); ?></title>
+	<title><?= get_bloginfo('name'); ?></title>
 
+	<link rel="icon" href="<?= path(); ?>images/favicon.ico">
+	<link rel="shortcut icon" href="<?= path(); ?>images/favicon.ico" type="image/x-icon" />
+
+    <?php wp_head(); ?>
 	
-	<link rel="icon" href="<?php $path->images(); ?>favicon.ico" type="">
-	<link rel="shortcut icon" href="<?php  $path->images(); ?>favicon.ico" type="image/x-icon" />
-
-    <?php // styles either here or the proper way via wp_enqueue_styles in functions.php ?>
-	<link rel="stylesheet" href="<?= $path->css(); ?>normalize.css" />
-	<link rel="stylesheet" href="<?= $path->css(); ?>styles.css" />
-	<?php wp_head(); ?>
-	<script>
-		if (window.navigator.msMaxTouchPoints) {
-			$( 'html' ).removeClass( 'no-touch' ).addClass( 'touch' );
-		}
-	</script>
-
 </head>
 <body id="#top" class="">
 	
