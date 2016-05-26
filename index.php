9ad97f5ef0f2db3d69fa23e6606d96d67930e70a
alpipego <alpipego@gmail.com>
2016-05-26 11:59:02 +0200

    basic loop

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php
 
-    get_header();
+get_header();
 
-    get_footer();
+if (have_posts()) :
+    while (have_posts()) :
+        the_post();
+        ?>
+        <h2><a href="<?= get_the_permalink(); ?>"><?= get_the_title(); ?></a></h2>
+        <div class="index-content">
+            <?= get_the_excerpt(); ?>
+        </div>
+        <?php
+    endwhile;
+endif;
+
+get_footer();
